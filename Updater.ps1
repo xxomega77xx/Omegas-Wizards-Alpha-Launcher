93c6521c298a73949b3f1e02610bd4c8d0048251
Matthew Carrier <matthew.s.carrier.civ@mail.mil>
2020-07-04 02:09:21 -0400

    updated

--- Updater.ps1
+++ Updater.ps1
@@ -1,3 +1,4 @@
-﻿$currentDesktopFilePath = (Get-ChildItem .\ | Where-Object Name -Match .exe).FullName
+﻿Stop-Process OmegaWizardsAlphaLauncher.exe -Force
+$currentDesktopFilePath = (Get-ChildItem .\ | Where-Object Name -Match OmegaWizardsAlphaLauncher.exe).FullName
 $serverExeUrl = "https://github.com/xxomega77xx/Omegas-Wizards-Alpha-Launcher/blob/master/bin/x64/OmegaWizardsAlphaLauncher.exe"
 Invoke-WebRequest -Uri $serverExeUrl -OutFile $currentDesktopFilePath
