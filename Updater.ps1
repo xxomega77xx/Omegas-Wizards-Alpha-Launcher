dd0be4ce1a5194b78245d210856ef7bea335297b
Matthew Carrier <matthew.s.carrier.civ@mail.mil>
2020-07-04 10:36:52 -0400

    updated

--- Updater.ps1
+++ Updater.ps1
@@ -1,4 +1,12 @@
-﻿Stop-Process OmegaWizardsAlphaLauncher.exe -Force
+﻿Write-Host "Stopping launcher process..."
+Stop-Process -Name OmegaWizardsAlphaLauncher -Force
+Start-Sleep 3
 $currentDesktopFilePath = (Get-ChildItem .\ | Where-Object Name -Match OmegaWizardsAlphaLauncher.exe).FullName
-$serverExeUrl = "https://github.com/xxomega77xx/Omegas-Wizards-Alpha-Launcher/blob/master/bin/x64/OmegaWizardsAlphaLauncher.exe"
+$serverExeUrl = "https://github.com/xxomega77xx/Omegas-Wizards-Alpha-Launcher/raw/master/bin/x64/OmegaWizardsAlphaLauncher.exe"
+Write-Host "Downloading latest update..."
 Invoke-WebRequest -Uri $serverExeUrl -OutFile $currentDesktopFilePath
+Start-Sleep 5
+Write-Host "Download Completed."
+Start-Sleep 5
+Write-Host "Launching updated launcher..."
+Start-Process -FilePath $currentDesktopFilePath
