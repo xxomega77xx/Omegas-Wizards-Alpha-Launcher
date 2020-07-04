79a62e4c2bc1966969b24cd1854d320100f300ea
Matthew Carrier <matthew.s.carrier.civ@mail.mil>
2020-07-04 19:15:24 -0400

    Added images folder

--- Updater.TempPoint.ps1
+++
@@ -1,3 +0,0 @@
-﻿$currentDesktopFilePath = (Get-ChildItem .\ | Where-Object Name -Match .exe).FullName
-$serverExeUrl = "https://github.com/xxomega77xx/Omegas-Wizards-Alpha-Launcher/blob/master/bin/x64/OmegaWizardsAlphaLauncher.exe"
-Invoke-WebRequest -Uri $serverExeUrl -OutFile $currentDesktopFilePath
